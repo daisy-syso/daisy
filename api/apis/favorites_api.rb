e5d15692ee29362bc08415b43ccff959d0daeb33
Theo <bbtfrr@gmail.com>
2014-08-29 14:15:19 +0800

    update the way to get random data for better performance

--- api/apis/favorites_api.rb
+++ api/apis/favorites_api.rb
@@ -17,11 +17,16 @@ class FavoritesAPI < Grape::API
   end
 
   RelatedClasses = [Drugs::Drug, Drugs::Drugstore, Hospitals::Hospital]
+  helpers do
+    def related_resources_count klass
+      Rails.cache.fetch([:api, :related, klass.name, :count]) do
+        klass.count
+      end
+    end
+  end
   get :related do
     relateds = (RelatedClasses * 3).sample(3).map do |klass|
-      klass.uncached do
-        klass.order("RAND()").first
-      end
+      klass.offset(Random.rand(related_resources_count(klass))).first
     end
     present! relateds, with: PolymorphicEntity
   end
