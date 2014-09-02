fb1ed792f0e42224f57f66f87173359bd1544ac2
Theo <bbtfrr@gmail.com>
2014-09-02 11:30:31 +0800

    Order::Statable concernalize

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,34 +1,6 @@
 class Order < ActiveRecord::Base
 
-  STATE = %w(opening pending paid completed canceled)
-  validates_inclusion_of :state, :in => STATE
-
-  # 添加 paid? completed? 等方法
-  STATE.each do |state|
-    define_method "#{state}?" do
-      self.state == state
-    end
-  end
-
-  # 状态迁移方法
-  def pend
-    update_attribute :state, 'pending' if opening?
-  end
-
-  # 只在 pending 状态可以 pay
-  def pay
-    update_attribute :state, 'paid' if pending?
-  end
-
-  # 只在 pending 和 paid 状态可以 complete
-  def complete
-    update_attribute :state, 'completed' if pendding? or paid?
-  end
-
-  # 只在 pending 和 paid 状态可以 cancel
-  def cancel
-    update_attribute :state, 'canceled' if pendding? or paid?
-  end
+  include Statable
 
   def name
     "#{item.name} x #{quantity}"
