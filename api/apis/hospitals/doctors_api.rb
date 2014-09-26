1795deffc3718cd4df97cbec9a9b0982c3ad0d26
Theo <bbtfrr@gmail.com>
2014-09-26 10:55:42 +0800

    mobile: update popover stylesheet

--- api/apis/hospitals/doctors_api.rb
+++ api/apis/hospitals/doctors_api.rb
@@ -16,7 +16,7 @@ class Hospitals::DoctorsAPI < ApplicationAPI
           title: "医院",
           meta: { filterable: true },
           children: proc {
-            Hospitals::Hospital.filters(params[:city])
+            Hospitals::Hospital.limit(100).filters(params[:city])
           }
         },
         hospital_room: { class: Hospitals::HospitalRoom, title: "医院科室" },
