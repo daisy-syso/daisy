7e726599ca9cf5a5c8e124f3c5e993125afc9868
zwt <357557688@qq.com>
2014-12-15 00:24:42 +0800

    add icon

--- app/models/diseases/common_disease.rb
+++ app/models/diseases/common_disease.rb
@@ -4,8 +4,17 @@ class Diseases::CommonDisease < ActiveRecord::Base
 	class << self
     include Filterable
 
-    define_nested_filter_method :filters do
-      self.all
+    def filters
+      self.all.group_by do |record|
+        record.name_initials.first
+      end.sort.map do |alphabet, records|
+        Hash.new.tap do |ret|
+          ret[:title] = alphabet
+          ret[:children] = generate_filters records
+        end
+      end
     end
+
+    define_cached_methods :filters
   end
 end
