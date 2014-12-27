72fe5730659661e8c8c201ea0097fc7af0af5ba3
zwt <357557688@qq.com>
2014-12-27 20:08:23 +0800

    相关医院只显示同类的

--- api/apis/related_resources_api.rb
+++ api/apis/related_resources_api.rb
@@ -7,6 +7,16 @@ class RelatedResourcesAPI < Grape::API
         klass.count
       end
     end
+
+    def hospital_type_id hospital_type_name
+      { '男科医院' => 1,
+        '整形医院' => 2,
+        '体检医院' => 3,
+        '中医医院' => 4,
+        '妇幼医院' => 5,
+        '牙科医院' => 6,
+        }[hospital_type_name]
+    end
   end
   get :related do
     relateds = (RelatedClasses * 3).sample(3).map do |klass|
@@ -16,11 +26,22 @@ class RelatedResourcesAPI < Grape::API
   end
 
   get :related_hospital do
-    relateds_onsale = []
+    id = hospital_type_id params[:hospital_type]
+    a = Hospitals::HospitalOnsale.all.map {|a| a.hospital_id}.compact
+    b = Hospitals::HospitalType.find(id).hospitals.map {|h| h.id}.compact
+    p "==========#{a & b}"
+    c = a & b
+
+    # 3.times do |i|
+    #   relateds_onsale << Hospitals::HospitalOnsale.offset(Random.rand(related_resources_count(Hospitals::HospitalOnsale))).first
+    # end
+    # relateds = relateds_onsale.map{|related_onsale| related_onsale.hospital }
+    relateds = []
     3.times do |i|
-      relateds_onsale << Hospitals::HospitalOnsale.offset(Random.rand(related_resources_count(Hospitals::HospitalOnsale))).first
+      random_number = Random.rand(c.count)
+      relateds << Hospitals::Hospital.where(id: c[random_number]).first
     end
-    relateds = relateds_onsale.map{|related_onsale| related_onsale.hospital }
+    p relateds
     present! relateds , with: Hospitals::HospitalEntity, hospital_onsales_no_type_id: true
   end
 
