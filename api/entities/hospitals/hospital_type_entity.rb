44fa21974f5220638ca72b7b3feaab773a164c9b
zwt <357557688@qq.com>
2015-01-18 22:54:50 +0800

    fix bug

--- api/entities/hospitals/hospital_type_entity.rb
+++ api/entities/hospitals/hospital_type_entity.rb
@@ -1,5 +1,8 @@
 class Hospitals::HospitalTypeEntity < ApplicationEntity
 
   expose :id, :name
+  expose :url do |instance, options|
+  	"#/list/hospitals/hospital_charges?hospital_parent_type=#{instance.parent_id}&hospital_type=#{instance.id}"
+  end
 
 end
