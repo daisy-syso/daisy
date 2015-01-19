4c50ba7066ae9571d6b4923da00a08ef2053523f
zwt <357557688@qq.com>
2015-01-20 00:51:38 +0800

    修改详情页面

--- api/entities/hospitals/hospital_entity.rb
+++ api/entities/hospitals/hospital_entity.rb
@@ -48,7 +48,8 @@ class Hospitals::HospitalEntity < Bases::PlaceEntity
 
   with_options if: { detail: true } do
     # expose :url
-
+    expose :telephone
+    expose :medical_insurance, :characteristic_departments
     expose :equipment_star, :skill_star, :service_star, :environment_star
     expose :equipment_desc, :skill_desc, :service_desc, :environment_desc
   end
