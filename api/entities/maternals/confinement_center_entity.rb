d73d248ff20ee14d9f559941431084bffe1d6c23
zwt <357557688@qq.com>
2015-03-15 22:02:14 +0800

    修改整形详情页

--- api/entities/maternals/confinement_center_entity.rb
+++ api/entities/maternals/confinement_center_entity.rb
@@ -1,3 +1,9 @@
 class Maternals::ConfinementCenterEntity < Bases::PlaceNIEntity
-
+	expose :price
+	with_options if: { detail: true } do
+    expose :url
+    expose :telephone
+    # expose :equipment_star, :skill_star, :service_star, :environment_star
+    # expose :equipment_desc, :skill_desc, :service_desc, :environment_desc
+  end
 end
--- api/entities/maternals/maternal_hall_entity.rb
+++ api/entities/maternals/maternal_hall_entity.rb
@@ -1,3 +1,7 @@
 class Maternals::MaternalHallEntity < Bases::PlaceNIEntity
-  
+  # expose :price
+	with_options if: { detail: true } do
+    # expose :url
+    expose :telephone
+  end
 end
