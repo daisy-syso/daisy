4b56c7d10a0856b4f378e8de8f9c414dc5c3fb28
zwt <357557688@qq.com>
2015-08-07 17:26:25 +0800

    修改养老服务的数据

--- api/entities/eldercares/nursing_room_entity.rb
+++ api/entities/eldercares/nursing_room_entity.rb
@@ -1,5 +1,7 @@
 class Eldercares::NursingRoomEntity < Bases::PlaceEntity
 
+	expose :bed, :target, :price, :nature, :mold, :service
+
   with_options if: { detail: true } do
   	expose :telephone
   end
