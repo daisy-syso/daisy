8a001076e2b5611f3ecd23459e799d4fbdd401de
zwt <357557688@qq.com>
2015-09-05 17:34:56 +0800

    修改筛选

--- api/apis/privileges/hospitals_api.rb
+++ api/apis/privileges/hospitals_api.rb
@@ -123,8 +123,7 @@ class Privileges::HospitalsAPI < ApplicationAPI
         has_return: form_switch_filters("返现"),
         # template: form_radio_array_filters(%w(不限 (含淋巴结清扫和取活检) 耻骨上前列腺切除术 耻骨后前列腺切除术 经会阴前列腺切除术 前列腺囊肿切除术 前列腺脓肿切开术 经尿道前列腺电切术(激光法) 经尿道前列腺电切术(电切法) 经尿道前列腺电切术(汽化法) 经尿道前列腺气囊扩张术 经尿道前列腺支架置入术 前列腺摘除术),
           # "当前主题精选"),
-        template: form_radio_array_filters_new("andrology", "当前主题精选"),
-        price_scope: form_price_scope_filters([500, 1000, 5000, 10000, 20000])
+        price_scope: form_price_scope_filters([500, 1000, 3000, 5000, 10000])
       },
       includes: [:hospital]
 
