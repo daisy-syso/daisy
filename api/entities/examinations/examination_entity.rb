a736bd4add496fb78bcaabd44c970fcacd1e0433
Felix <hefengjs@126.com>
2015-09-05 20:43:30 +0800

    fix api not same problem

--- api/entities/examinations/examination_entity.rb
+++ api/entities/examinations/examination_entity.rb
@@ -1,11 +1,11 @@
 class Examinations::ExaminationEntity < Bases::ItemEntity
 
   expose :name, :price, :image_url
-  expose :examination_type, using: Examinations::ExaminationTypeEntity, as: :types
+  expose :examination_type, using: Examinations::ExaminationTypeEntity
 
   with_options if: { detail: true } do
     expose :address, :hospital_name
-    expose :examination_details, using: Examinations::ExaminationDetailEntity, as: :details
+    expose :examination_details, using: Examinations::ExaminationDetailEntity
   end
 
   expose :template do |instance, options|
