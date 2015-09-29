eb0668b00b97c331cae80385fafed034019b69d5
Felix <hefengjs@126.com>
2015-09-29 11:30:05 +0800

    get informations by page

--- api/apis/informations/health_infors_api.rb
+++ api/apis/informations/health_infors_api.rb
@@ -5,6 +5,9 @@ class Informations::HealthInforsAPI < ApplicationAPI
       information_types = Informations::InformationType.select("id, name").where(parent_id: [nil, ''])
       information_type = Informations::InformationType.where(id: params[:type]) if params[:type]
       information_type ||= information_types
+      information_type.each do |it|
+        it.latest_informations = it.informations.order("str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc").page(params[:page]).per(8)
+      end
       present title: "健康资讯"
       present information_types: information_types
       present :data, information_type, with: Informations::InformationTypeEntity
--- app/models/informations/information_type.rb
+++ app/models/informations/information_type.rb
@@ -2,9 +2,7 @@ class Informations::InformationType < ActiveRecord::Base
 	has_many :informations, class_name: "Informations::Information", foreign_key: 'information_type_id'
 	self.table_name = "information_type"
 
-	def latest_informations
-		self.informations.order("str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc").limit(8)
-	end
+	attr_accessor :latest_informations
 
 	def children_items
 		Informations::InformationType.select('id, name').where(parent_id: self.id)
