de6d5b5b78cf275fd078d86f57554b63c358f920
leif <leif@beansmile.com>
2015-09-29 22:45:22 +0800

    uncomment

--- app/models/informations/information.rb
+++ app/models/informations/information.rb
@@ -1,7 +1,7 @@
 class Informations::Information < ActiveRecord::Base
 	belongs_to :information_type, class_name: "Informations::InformationType", foreign_key: 'information_type_id'
 	self.table_name = "informations"
-	# include Reviewable
+	include Reviewable
 
 	def recommended
 		Informations::Information.select('id, name').where.not(id: self.id).order("str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc").limit(8)
