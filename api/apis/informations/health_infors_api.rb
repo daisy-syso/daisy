ef2ea61733fd2862d3b322c1f2e483a4565977e3
Felix <hefengjs@126.com>
2015-10-12 10:10:51 +0800

    资讯查看所有

--- api/apis/informations/health_infors_api.rb
+++ api/apis/informations/health_infors_api.rb
@@ -2,11 +2,13 @@ class Informations::HealthInforsAPI < ApplicationAPI
 
   namespace :health_infors do
     get do
-      information_types = Informations::InformationType.select("id, name").where(parent_id: [nil, '']).order("created_at desc")
+      information_types = Informations::InformationType.select("id, name, parent_id").where(parent_id: [nil, '']).order("created_at desc")
       information_type = Informations::InformationType.where(id: params[:type]) if params[:type]
       information_type ||= information_types
       information_type.each do |it|
-        it.latest_informations = it.informations.order("str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc").page(params[:page]).per(8)
+        it_ids = it.parent_id.blank? ? [] : it.children_items.map(&:id)
+        it_ids << it.id
+        it.latest_informations = Informations::Information.where(information_type_id: it_ids).order("str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc").page(params[:page]).per(8)
       end
       present title: "健康资讯"
       present information_types: information_types
