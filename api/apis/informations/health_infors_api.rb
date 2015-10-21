88930c537b26935c413c60ca0b293f9bdc02ccac
Felix <hefengjs@126.com>
2015-10-21 12:44:36 +0800

    fix bug

--- api/apis/informations/health_infors_api.rb
+++ api/apis/informations/health_infors_api.rb
@@ -9,11 +9,11 @@ class Informations::HealthInforsAPI < ApplicationAPI
         top_infors = "(select * from informations where information_type_id = #{parent_id} and is_top is true order by str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc)"
         if it.parent_id.blank?
           ids = it.children_items.map(&:id) + [it.id]
-          select_infos = "(select * from informations where id in (#{ids.join(',')}) and is_top is not true order by str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc)"
+          select_infos = "(select * from informations where information_type_id in (#{ids.join(',')}) and is_top is not true order by str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc)"
         else
           part_infors = "(select * from informations where information_type_id = #{it.id} and is_top is not true order by str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc)"
           ids = (it.parent_item.children_items.map(&:id) + [it.parent_item.id]).delete_if{|i| i == it.id}
-          select_infos = part_infors + " union " + "(select * from informations where id in (#{ids.join(',')}) and is_top is not true order by str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc)"
+          select_infos = part_infors + " union " + "(select * from informations where information_type_id in (#{ids.join(',')}) and is_top is not true order by str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc)"
         end
         info_sql = "select * from ( " + top_infors + " union " + select_infos + " ) as infors limit 8 offset #{((params[:page] || 1).to_i - 1) * 8}"
         it.latest_informations = Informations::Information.find_by_sql(info_sql)
