95bc1852ff532aff451586aafe1cba84a595941d
Felix <hefengjs@126.com>
2015-10-22 14:58:27 +0800

    fix bug

--- api/apis/informations/health_infors_api.rb
+++ api/apis/informations/health_infors_api.rb
@@ -6,16 +6,16 @@ class Informations::HealthInforsAPI < ApplicationAPI
       information_type = params[:type].blank? ? information_types : Informations::InformationType.where(id: params[:type])
       information_type.each do |it|
         parent_id = it.parent_id || it.id
-        top_infors = "(select * from informations where information_type_id = #{parent_id} and is_top is true and types = 0 order by str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc)"
+        top_infors = "(select *, 1 as rank from informations where information_type_id = #{parent_id} and is_top is true and types = 0)"
         if it.parent_id.blank?
           ids = it.children_items.map(&:id) + [it.id]
-          select_infos = "(select * from informations where information_type_id in (#{ids.join(',')}) and is_top is not true and types = 0 order by str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc)"
+          select_infos = "(select *, 2 as rank from informations where information_type_id in (#{ids.join(',')}) and is_top is not true and types = 0)"
         else
-          part_infors = "(select * from informations where information_type_id = #{it.id} and is_top is not true and types = 0 order by str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc)"
+          part_infors = "(select *, 3 as rank from informations where information_type_id = #{it.id} and is_top is not true and types = 0)"
           ids = (it.parent_item.children_items.map(&:id) + [it.parent_item.id]).delete_if{|i| i == it.id}
-          select_infos = part_infors + " union " + "(select * from informations where information_type_id in (#{ids.join(',')}) and is_top is not true and types = 0 order by str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc)"
+          select_infos = part_infors + " union " + "(select *, 4 as rank from informations where information_type_id in (#{ids.join(',')}) and is_top is not true and types = 0)"
         end
-        info_sql = "select * from ( " + top_infors + " union " + select_infos + " ) as infors limit 8 offset #{((params[:page] || 1).to_i - 1) * 8}"
+        info_sql = "select * from ( " + top_infors + " union " + select_infos + " ) as infors order by rank asc, str_to_date(infors.created_at,'%Y-%m-%d %H:%i:%s') desc limit 8 offset #{((params[:page] || 1).to_i - 1) * 8}"
         it.latest_informations = Informations::Information.find_by_sql(info_sql)
       end
       present title: "健康资讯"
