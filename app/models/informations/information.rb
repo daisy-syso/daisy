8cda64644b3b13a91444c452695318047dc0309b
menghuanwd <651019063@qq.com>
2015-12-06 09:38:14 +0800

    修复资讯默认scope bug

--- app/models/informations/information.rb
+++ app/models/informations/information.rb
@@ -12,14 +12,14 @@ class Informations::Information < ActiveRecord::Base
   scope :selected, -> { unscope(:where).where types: 3 }  #精选资讯
 
   def recommended
-    Informations::Information.where(:unscope).select('id, name').where.not(id: self.id).order("str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc").limit(8)
+    Informations::Information.unscope(:where).select('id, name').where.not(id: self.id).order("str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc").limit(8)
   end
 
   def hot_images
-    Informations::Information.where(:unscope).select('id, name, image_url').where.not(id: self.id, image_url: nil).order("str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc").limit(8)
+    Informations::Information.unscope(:where).select('id, name, image_url').where.not(id: self.id, image_url: nil).order("str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc").limit(8)
   end
 
   def selected
-    Informations::Information.where(:unscope).select('id, name').where.not(id: self.id).order("read_count desc").limit(8)
+    Informations::Information.unscope(:where).select('id, name').where.not(id: self.id).order("read_count desc").limit(8)
   end
 end
