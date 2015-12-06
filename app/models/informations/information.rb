46470cbf6c7728b7ebcb041e32ae4b40797515ff
menghuanwd <651019063@qq.com>
2015-12-06 09:34:16 +0800

    修复资讯默认scope bug

--- app/models/informations/information.rb
+++ app/models/informations/information.rb
@@ -6,20 +6,20 @@ class Informations::Information < ActiveRecord::Base
 
   include Reviewable
 
-  default_scope { where(types: 0) }   # 普通资讯
+  # default_scope { where(types: 0) }   # 普通资讯
   scope :guessed, -> { unscope(:where).where types: 1 }  #猜你喜欢资讯
   scope :recommended, -> { unscope(:where).where types: 2 }  #推荐资讯
   scope :selected, -> { unscope(:where).where types: 3 }  #精选资讯
 
   def recommended
-    Informations::Information.select('id, name').where.not(id: self.id).order("str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc").limit(8)
+    Informations::Information.where(:unscope).select('id, name').where.not(id: self.id).order("str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc").limit(8)
   end
 
   def hot_images
-    Informations::Information.select('id, name, image_url').where.not(id: self.id, image_url: nil).order("str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc").limit(8)
+    Informations::Information.where(:unscope).select('id, name, image_url').where.not(id: self.id, image_url: nil).order("str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc").limit(8)
   end
 
   def selected
-    Informations::Information.select('id, name').where.not(id: self.id).order("read_count desc").limit(8)
+    Informations::Information.where(:unscope).select('id, name').where.not(id: self.id).order("read_count desc").limit(8)
   end
 end
