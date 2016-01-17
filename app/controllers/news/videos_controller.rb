87b9c5c9ce6ce4b37cb2b0ad2a858967f4d2d640
menghuanwd <651019063@qq.com>
2016-01-17 10:17:37 +0800

    update video play

--- app/controllers/news/videos_controller.rb
+++ app/controllers/news/videos_controller.rb
@@ -1,5 +1,6 @@
 class News::VideosController < NewsController
-  before_action :set_video_category, only: [:index]
+  before_action :set_video_category, only: [:index, :show]
+  before_action :set_video, only: [:show]
   
   def index
     @videos = @video_category.videos.where.not(swf: nil).order("created_at desc").page(params[:page]).per(5)
@@ -13,4 +14,9 @@ class News::VideosController < NewsController
     def set_video_category
       @video_category = VideoCategory.find(params[:video_category_id])
     end
+
+    def set_video
+      @video = @video_category.videos.find(params[:id])
+    end
 end
+
